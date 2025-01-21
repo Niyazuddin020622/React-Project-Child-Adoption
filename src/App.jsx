3869472b128b6faed2e0652107c970276e98623b
Niyazuddin Ansari <ansariniyazuddin87@gmail.com>
2025-01-21 21:50:05 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./components/Home";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
-function App() {
- 
 
+const App = () => {
   return (
-    <>
-     
-    </>
-  )
-}
+    <Router>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      
+    </Routes>
+    <Footer />
+  </Router>
+  );
+};
 
-export default App
+export default App;
