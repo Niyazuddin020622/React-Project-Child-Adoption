236384013d3060514508ca6c8cd10c2eda2b7ec3
27-madhu <mkumari68163@gmail.com>
2025-01-28 00:10:02 +0530

    update code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="text-white text-center text-lg-start " style={{background:"#06bbcc"}}>
+    <footer className="text-white text-center text-lg-start  bg-dark" >
       {/* Grid container */}
       <div className="p-4">
         {/* Grid row */}
