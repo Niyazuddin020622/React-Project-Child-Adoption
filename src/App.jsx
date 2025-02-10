303e2da109bd78dc7eb522cca7bf3ca5f702ee65
Niyazuddin Ansari <ansariniyazuddin87@gmail.com>
2025-02-11 00:35:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 // import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ScrollToTop from "./pages/ScrollToTop";
-import Home from "./pages/Home";
-import About from "./pages/About";
+import ScrollToTop from "./components/ScrollToTop";
+// import Home from "./pages/Home";
+import About from "./components/About";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Contact from "./pages/Contact";
-import Adoption from "./pages/AdoptionResources";
-import AvailableChildren from "./pages/AvailableChildren";
-import ChildDetails from "./pages/ChildDetails";
+import Contact from "./components/Contact";
+import Adoption from "./components/AdoptionResources";
+import AvailableChildren from "./components/AvailableChildren";
+import ChildDetails from "./components/ChildDetails";
 import Gallery from "./GalleryPages/Gallery";
-import AdoptNow from  "./pages/AdoptNow";
+import AdoptNow from  "./components/AdoptNow";
 
 
 
@@ -21,7 +21,7 @@ const App = () => {
       <ScrollToTop />
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/adoption-resources" element={<Adoption />} />
