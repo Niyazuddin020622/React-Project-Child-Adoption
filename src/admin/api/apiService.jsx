8ad17acdecc29c623cef4d20d7408bdc73443505
27-madhu <mkumari68163@gmail.com>
2025-03-01 15:59:37 +0530

    apiservice add

---
+++ src/admin/api/apiService.jsx
@@ -0,0 +1,19 @@
+export const fetchChildren = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/children");
+      return response.json();
+    } catch (error) {
+      console.error("Error fetching children:", error);
+      return [];
+    }
+  };
+  
+  export const fetchUsers = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/admin");
+      return response.json();
+    } catch (error) {
+      console.error("Error fetching users:", error);
+      return [];
+    }
+  };
